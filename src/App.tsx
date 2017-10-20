8a56b4cfad5df86363806affe4218cab7cd001e1
Arvind Narayanan <anarayanan@dvibes.co>
2017-10-20 22:36:11 +0530

    Remove svg and video

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@
 import Navigator, { Types, NavigatorDelegateSelector as DelegateSelector } from 'reactxp-navigation';
 import RX = require('reactxp');
 
-import MainPanel = require('./MainPanel');
-import SecondPanel = require('./SecondPanel');
-
-enum NavigationRouteId {
-    MainPanel,
-    SecondPanel
-}
-
 const styles = {
     // Standard navigator style should be an object. So we have to disable caching here.
     navCardStyle: RX.Styles.createViewStyle({
@@ -21,51 +13,8 @@ const styles = {
 };
 
 class App extends RX.Component<{}, null> {
-    private _navigator: Navigator;
-
-    componentDidMount() {
-        this._navigator.immediatelyResetRouteStack([{
-            routeId: NavigationRouteId.MainPanel,
-            sceneConfigType: Types.NavigatorSceneConfigType.Fade
-        }]);
-    }
-
     render() {
-        return (
-            <Navigator
-                ref={ this._onNavigatorRef }
-                renderScene={ this._renderScene }
-                cardStyle={ styles.navCardStyle }
-                delegateSelector={ DelegateSelector }
-            />
-        );
-    }
-
-    private _onNavigatorRef = (navigator: Navigator) => {
-        this._navigator = navigator;
-    }
-
-    private _renderScene = (navigatorRoute: Types.NavigatorRoute) => {
-        switch (navigatorRoute.routeId) {
-            case NavigationRouteId.MainPanel:
-                return <MainPanel onPressNavigate={ this._onPressNavigate } />;
-
-            case NavigationRouteId.SecondPanel:
-                return <SecondPanel onNavigateBack={ this._onPressBack } />;
-        }
-
-        return null;
-    }
-
-    private _onPressNavigate = () => {
-        this._navigator.push({
-            routeId: NavigationRouteId.SecondPanel,
-            sceneConfigType: Types.NavigatorSceneConfigType.FloatFromRight
-        });
-    }
-
-    private _onPressBack = () => {
-        this._navigator.pop();
+        return (<RX.Text>Hello world</RX.Text>);
     }
 }
 
